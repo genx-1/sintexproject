0a22bcae8fca020b781441c543ebf6eee21f6742
genx-1 <komalmakke@gmail.com>
2025-05-30 19:30:50 +0530

    third commit

--- src/test/java/Day30/prog1.java
+++ src/test/java/Day30/prog1.java
@@ -3,6 +3,12 @@ package Day30;
 public class prog1 {
 private void syso() {
 	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub
 
 }
 }
